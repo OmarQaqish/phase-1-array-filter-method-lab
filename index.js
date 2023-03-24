b828ccc5e01e7bdc47693b426344c3e196957053
Omar Qaqish <omqaqish@gmail.com>
2023-03-25 00:57:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+}
+    
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(str.toLowerCase()));
+}
+    
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
